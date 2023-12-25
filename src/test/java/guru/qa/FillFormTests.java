d4bc596ca86b64dde1ae2e095419c49777f35e21
gestrigh <rt.tim433@gmail.com>
2023-12-25 17:47:16 +0300

    Fix 1.1

--- src/test/java/guru/qa/FillFormTests.java
+++ src/test/java/guru/qa/FillFormTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -16,23 +17,21 @@ public class FillFormTests extends BaseTest {
         Selenide.executeJavaScript("$('footer').remove()");
     }
     @Test
-            void fillFormTest() {
-        //Не понял что ему не понравилось, пробел в placeholder? org.openqa.selenium.InvalidSelectorException: invalid selector: An invalid or illegal selector was specified
-        //$("[placeholder=First]").setValue("Roman");
+    void fillFormTest() {
         $("#firstName").setValue("Roman");
         $("#lastName").setValue("Timofeev");
         $("#userEmail").setValue("test@gmail.com");
-        $("#gender-radio-1").parent().click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue("0123456789");
 
         //DateOfBirthday
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOptionByValue("0");
+        $(".react-datepicker__month-select").selectOption("January");
         $(".react-datepicker__year-select").selectOption("1998");
         $(".react-datepicker__day--011").click();
 
         //Subjects
-        $("[id=subjectsInput]").setValue("Anime, Gaming, Coding, Anime");
+        $("#subjectsInput").setValue("Arts").pressEnter();
 
         //Hobbies
         $("#hobbies-checkbox-1").parent().click();
@@ -59,6 +58,7 @@ public class FillFormTests extends BaseTest {
         $(".table").shouldHave(text("test@gmail.com"));
         $(".table").shouldHave(text("0123456789"));
         $(".table").shouldHave(text("11 January,1998"));
+        $(".table").shouldHave(text("Arts"));
         $(".table").shouldHave(text(imgName));
         $(".table").shouldHave(text("Current Address"));
         $(".table").shouldHave(text("NCR Delhi"));
