e0917c73eb58040d1be72697070dd183c2ba8743
gestrigh <rt.tim433@gmail.com>
2024-01-12 02:32:02 +0300

    hw 6

---
+++ src/test/java/guru/qa/GithubEnterprizePageTest.java
@@ -0,0 +1,19 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class GithubEnterprizePageTest {
+    @Test
+    void githubEnterprizePageTest() {
+        open("https://github.com/");
+        $(byTagAndText("button", "Solutions")).hover();
+        $("a[href='/enterprise']").click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered")).shouldBe(visible);
+    }
+}
