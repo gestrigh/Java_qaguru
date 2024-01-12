7703d413081039b19df62e952eb4abe449f7aa26
gestrigh <rt.tim433@gmail.com>
2024-01-12 15:42:57 +0300

    hw 6

---
+++ src/test/java/guru/qa/DragAndDropTest.java
@@ -0,0 +1,31 @@
+package guru.qa;
+
+import com.codeborne.selenide.DragAndDropOptions;
+import com.codeborne.selenide.selector.ByTagAndText;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+    @Test
+    void actionsTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        actions()
+                .moveToElement($("#column-a"))
+                .clickAndHold()
+                .moveToElement($("#column-b"))
+                .release()
+                .perform();
+        $("#column-a").$(byTagAndText("header","B")).shouldBe(visible);
+    }
+    @Test
+    void dragAndDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+        $("#column-b").$(byTagAndText("header","A")).shouldBe(visible);
+        $("#column-a").$(byTagAndText("header","B")).shouldBe(visible);
+    }
+}
+
