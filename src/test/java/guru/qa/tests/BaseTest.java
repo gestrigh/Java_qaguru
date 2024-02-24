06310538d1ae842e2c8767ebfd8ea69cbb953255
gestrigh <rt.tim433@gmail.com>
2024-02-24 17:41:39 +0300

    hw 13

--- src/test/java/guru/qa/tests/BaseTest.java
+++ src/test/java/guru/qa/tests/BaseTest.java
@@ -1,17 +1,8 @@
 package guru.qa.tests;
 
 import com.codeborne.selenide.Configuration;
-import io.qameta.allure.Attachment;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Map;
-
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-import static org.openqa.selenium.remote.HttpSessionId.getSessionId;
 
 public class BaseTest {
     @BeforeAll
@@ -22,9 +13,5 @@ public class BaseTest {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.browser = "chrome";
         Configuration.browserVersion = "100.0";
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("selenoid:options", Map.<String, Object>of("enableVNC", true, "enableVideo", true));
-//        Configuration.browserCapabilities = capabilities;
-
     }
 }
