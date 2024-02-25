642df47a4721e52cdaf12ca6fc03f247f73ee8c1
gestrigh <rt.tim433@gmail.com>
2024-02-25 16:40:52 +0300

    hw 15

---
+++ src/test/java/guru/qa/config/ProjectConfig.java
@@ -0,0 +1,15 @@
+package guru.qa.config;
+
+import org.aeonbits.owner.Config;
+@Config.Sources("classpath:config/${env}.properties")
+public interface ProjectConfig extends Config   {
+    @Key("student.name")
+    String studentName();
+    @Key("student.last.name")
+    String studentLastName();
+        @Key("student.email")
+    String studentEmail();
+        @Key("student.number")
+    String studentNumber();
+
+}
---
+++ src/test/java/guru/qa/tests/ParameterizedJenkinsTest.java
@@ -0,0 +1,33 @@
+package guru.qa.tests;
+
+import guru.qa.config.ProjectConfig;
+import guru.qa.pages.RegistrationPage;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ParameterizedJenkinsTest extends BaseTest {
+    RegistrationPage registrationPage = new RegistrationPage();
+    ProjectConfig projectConfig;
+    @BeforeEach
+            void setConfig(){
+        System.setProperty("env", "test");
+        projectConfig = ConfigFactory.create(ProjectConfig.class);
+    }
+    @Test
+    @DisplayName("Параметризованная форма с не полными данными")
+    void parameterizedRegistrationTest(){
+        registrationPage.openPage()
+                .setFirstName(projectConfig.studentName())
+                .setLastName(projectConfig.studentLastName())
+                .setUserEmail(projectConfig.studentEmail())
+                .setGender("Male")
+                .setUserNumber(projectConfig.studentNumber())
+                .clickSubmit()
+                .checkResult("Student Name", projectConfig.studentName() + " " + projectConfig.studentLastName())
+                .checkResult("Student Email", projectConfig.studentEmail())
+                .checkResult("Gender", "Male")
+                .checkResult("Mobile", projectConfig.studentNumber());
+    }
+}
