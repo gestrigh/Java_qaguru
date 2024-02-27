c45a16417bde8a1081a3c650d54205b119739b0d
gestrigh <rt.tim433@gmail.com>
2024-02-27 20:49:27 +0300

    hw 15 fix

--- src/test/java/guru/qa/tests/BaseTest.java
+++ src/test/java/guru/qa/tests/BaseTest.java
@@ -2,11 +2,8 @@ package guru.qa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import guru.qa.config.DriverConfig;
-import guru.qa.config.ProjectConfig;
 import guru.qa.helpers.Attach;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
